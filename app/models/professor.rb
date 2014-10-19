292f3f1379959427297a7c19cba81ccad1c73f8f
Ubuntu <ubuntu@ip-172-31-19-212.us-west-2.compute.internal>
2014-10-19 06:02:21 +0000

    Fixes preventing the creating of an empty rating while creating a new professor.  Original code prevented the creation of all ratings while crating a new professor.  Dumb mistake; cut and paste from manual w/o making correct changes.

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,7 +1,7 @@
 class Professor < ActiveRecord::Base
 
   has_many :ratings, dependent: :destroy
-  accepts_nested_attributes_for :ratings, reject_if: lambda { |attributes| attributes['kind'].blank?}
+  accepts_nested_attributes_for :ratings, reject_if: lambda { |attributes| attributes['course'].blank? || attributes['comment'].blank? || attributes['rating'].blank?}
 
   validates :first, presence: true
   validates :last, presence: true
